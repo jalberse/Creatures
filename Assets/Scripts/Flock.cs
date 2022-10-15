3801999de2b7977e5b8339eb2a5266bceb718148
John Alberse <alberse.john@gmail.com>
2022-10-15 16:50:36 -0500

    Add basic flocking
    Adds basic flocking boids with centering, avoidance, and
    velocity matching. Boids behave according to these forces,
    calculating acceleration and updating their positions and
    velocities according to first degree Euler integration.

---
+++ Assets/Scripts/Flock.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    float speed;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(FlockManager.FM.minSpeed, FlockManager.FM.maxSpeed);
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(this.velocity * Time.deltaTime);
+        this.velocity += this.CalculateAcceleration() * Time.deltaTime;
+    }
+
+    Vector3 CalculateAcceleration()
+    {
+        GameObject[] boids = FlockManager.FM.boids;
+
+        Vector3 acceleration = Vector3.zero;
+        foreach(GameObject boid in boids) {
+            if(boid == this.gameObject)
+            {
+                continue;
+            }
+            float distance = Vector3.Distance(boid.transform.position, this.transform.position);
+            if( distance > FlockManager.FM.neighbourDistance)
+            {
+                continue;
+            }
+
+            // The other boid is close enough to influence this boid.
+            // Calculate the acceleration on this boid due to the other boid.
+            Vector3 u = (boid.transform.position - this.transform.position).normalized;
+            // Avoidance
+            acceleration += u * (float)(-1.0 * FlockManager.FM.avoidanceFactor / Mathf.Pow(distance, 2.0f));
+            // Centering
+            acceleration += u * distance * FlockManager.FM.centeringFactor;
+            // Velocity Matching 
+            Flock otherFlock = boid.GetComponent<Flock>();
+            acceleration += FlockManager.FM.velocityMatchingFactor * otherFlock.velocity - this.velocity;
+        }
+
+        // We're operating in 2D, so ensure we don't move on z.
+        acceleration.z = 0.0f;
+        return acceleration;
+    }
+}
---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager FM;
+
+    public GameObject boidPrefab;
+    public int numBoids = 20;
+    public GameObject[] boids;
+    public Vector3 boidLimits = new Vector3(5, 5, 0);
+
+    [Header ("Boid Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+    [Range(1.0f, 10.0f)]
+    public float neighbourDistance;
+    [Range(0.0f, 1.0f)]
+    public float avoidanceFactor;
+    [Range(0.0f, 1.0f)]
+    public float centeringFactor;
+    [Range(0.0f, 1.0f)]
+    public float velocityMatchingFactor;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boids = new GameObject[numBoids];
+        for(int i = 0; i < numBoids; i++)
+        {
+            Vector3 pos = this.transform.position = new Vector3
+                (
+                Random.Range(-boidLimits.x, boidLimits.x),
+                Random.Range(-boidLimits.x, boidLimits.x),
+                0
+                );
+            boids[i] = Instantiate(boidPrefab, pos, Quaternion.identity);
+        }
+        FM = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
